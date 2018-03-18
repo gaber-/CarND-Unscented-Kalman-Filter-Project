423f88540a051d8cac65dfc5ae1ab256a0ad972b
gaber <gaber@localhost.localdomain>
2018-03-18 15:39:21 +0100

    define rmse

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,25 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if (estimations.size() != ground_truth.size() || estimations.size() ==0)
+      return rmse;
   /**
   TODO:
     * Calculate the RMSE here.
   */
+  for (int i = 0; i<estimations.size(); i++){
+      VectorXd diff (4);
+      diff = estimations[i]-ground_truth[i];
+      diff = diff.array().pow(2);
+      rmse += diff;
+  }
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+  cout << "rmse :" << rmse(0) << "|" << rmse (1) << "|" << rmse(2) << "|" << rmse(3) << endl;
+    cout << "rmse" << endl;
+
+  return rmse;
 }
